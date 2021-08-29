df63cdbc2e699289e2f9ce1d7d122a28dd731b8a
ana <anacspedro@gmail.com>
2021-08-29 20:54:55 -0300

    insercao styled.js

---
+++ src/styled.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const Corpo = styled.body`
+background-color: #48dbfb;
+`
+
+export const Title = styled.h1`
+font-family: 'Rubik', sans-serif;
+font-weight: 700;
+font-size: 60px;
+color: #576574;
+text-align: left;
+`
+
+export const Conteiner = styled.fieldset`
+display: flex;
+border: none;
+margin: -10px;
+padding: -10px;
+`
+
+export const Cabecalho = styled.h2`
+font-size: 15px;
+font-weight: bold;
+border-bottom: 3px solid #576574;
+margin-top: 10px;
+`
+export const Dtitle = styled.div`
+width: 600px;
+margin-top: -10px;
+padding-right: 25px;
+display: flex;
+align-items: center;
+justify-content: flex-end;
+background-color: #ddd;
+border-color: #ddd;
+border-radius: 0 10px 10px 0;
+box-shadow: 1px 2px 2px #576574;
+`;
+
+export const Form = styled.div`
+font-family: 'Roboto', sans-serif;
+paddin: -10px;
+margin: auto;
+padding: -10px;
+`   
+
+export const Asterisco = styled.span`
+color: red;
+`
+
+export const Entrada = styled.input`
+border-radius: 5px;
+padding: 3px;
+margin-left: 8px;
+border: 1px solid #ddd;
+border-radius: 15px;
+`
+
+export const Selecao = styled.select`
+padding: 3px;
+margin-left: 8px;
+border: 1px solid #ddd;
+border-radius: 15px;
+`
